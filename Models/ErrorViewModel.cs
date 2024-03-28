a153b9011ad2218dbde81b490ffd5c01f4550ebe
Kumail Doctor <dkumail@my.centennialcollege.ca>
2024-03-28 14:09:02 -0400

    Initial commit: Set up ASP.NET web application project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quiz3SampleProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
